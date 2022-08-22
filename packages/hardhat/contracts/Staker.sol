b21444160538e98ef587827d9a71486877906894
Rick Steawart <rickstewartdev@gmail.com>
2022-08-23 00:06:06 +0100

    contract updated

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -5,31 +5,43 @@ import "hardhat/console.sol";
 import "./ExampleExternalContract.sol";
 
 contract Staker {
+    ExampleExternalContract public exampleExternalContract;
 
-  ExampleExternalContract public exampleExternalContract;
+    mapping(address => uint256) public balances;
+    mapping(address => uint256) public depositTimestamps;
 
-  constructor(address exampleExternalContractAddress) {
-      exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
-  }
+    uint256 public constant rewarRatePerBlock = 0.1 ether;
+    uint256 public withdrawalDeadline = block.timestamp + 120 seconds;
+    uint256 public claimDeadline = block.timestamp + 240 seconds;
+    uint256 public currentBlock = 0;
 
-  // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
-  // ( Make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
+    event Stake(address indexed sender, uint256 amount);
+    event Received(address, uint256);
+    event Execute(address indexed sender, uint256 amount);
 
+    constructor(address exampleExternalContractAddress) {
+        exampleExternalContract = ExampleExternalContract(
+            exampleExternalContractAddress
+        );
+    }
 
-  // After some `deadline` allow anyone to call an `execute()` function
-  // If the deadline has passed and the threshold is met, it should call `exampleExternalContract.complete{value: address(this).balance}()`
+    function withdrawalTimeLeft()
+        public
+        view
+        returns (uint256 _withdrawalTimeLeft)
+    {
+        if (block.timestamp >= withdrawalDeadline) {
+            return 0;
+        } else {
+            return (withdrawalDeadline - block.timestamp);
+        }
+    }
 
-
-  // If the `threshold` was not met, allow everyone to call a `withdraw()` function
-
-
-  // Add a `withdraw()` function to let users withdraw their balance
-
-
-  // Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
-
-
-  // Add the `receive()` special function that receives eth and calls stake()
-
-
+    function claimPeriodLeft() public view returns (uint256 _claimPeriodLeft) {
+        if (block.timestamp >= claimDeadline) {
+            return 0;
+        } else {
+            return (claimDeadline - block.timestamp);
+        }
+    }
 }
