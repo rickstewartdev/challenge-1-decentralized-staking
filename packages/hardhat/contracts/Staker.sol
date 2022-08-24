f846b58ad32b3c5b56952f6cedb88eeec1ded989
Rick Steawart <rickstewartdev@gmail.com>
2022-08-24 18:59:51 +0100

    finished

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -10,7 +10,7 @@ contract Staker {
     mapping(address => uint256) public balances;
     mapping(address => uint256) public depositTimestamps;
 
-    uint256 public constant rewarRatePerBlock = 0.1 ether;
+    uint256 public constant rewardRatePerBlock = 0.1 ether;
     uint256 public withdrawalDeadline = block.timestamp + 120 seconds;
     uint256 public claimDeadline = block.timestamp + 240 seconds;
     uint256 public currentBlock = 0;
@@ -18,6 +18,32 @@ contract Staker {
     event Stake(address indexed sender, uint256 amount);
     event Received(address, uint256);
     event Execute(address indexed sender, uint256 amount);
+
+    modifier withdrawalDeadlineReached(bool requireReached) {
+        uint256 timeRemaining = withdrawalTimeLeft();
+        if (requireReached) {
+            require(timeRemaining == 0, "Withdrawal period is not reached yet");
+        } else {
+            require(timeRemaining > 0, "Withdrawal period has been reached");
+        }
+        _;
+    }
+
+    modifier claimDeadlineReached(bool requireReached) {
+        uint256 timeRemaining = claimPeriodLeft();
+        if (requireReached) {
+            require(timeRemaining == 0, "Claim deadline is not reached yet");
+        } else {
+            require(timeRemaining > 0, "Claim deadline has been reached");
+        }
+        _;
+    }
+
+    modifier notCompleted() {
+        bool completed = exampleExternalContract.completed();
+        require(!completed, "Stake already completed!");
+        _;
+    }
 
     constructor(address exampleExternalContractAddress) {
         exampleExternalContract = ExampleExternalContract(
@@ -43,5 +69,61 @@ contract Staker {
         } else {
             return (claimDeadline - block.timestamp);
         }
+    }
+
+    function stake()
+        public
+        payable
+        withdrawalDeadlineReached(false)
+        claimDeadlineReached(false)
+    {
+        balances[msg.sender] = balances[msg.sender] + msg.value;
+        depositTimestamps[msg.sender] = block.timestamp;
+        emit Stake(msg.sender, msg.value);
+    }
+
+    function withdraw()
+        public
+        withdrawalDeadlineReached(true)
+        claimDeadlineReached(false)
+        notCompleted
+    {
+        require(balances[msg.sender] > 0, "You have no balances to withdraw");
+        uint256 individualBalance = balances[msg.sender];
+        uint256 indBalanceRewards = individualBalance +
+            ((block.timestamp - depositTimestamps[msg.sender]) *
+                rewardRatePerBlock);
+        balances[msg.sender] = 0;
+
+        // Transfer all ETH via call! (not transfer) cc: https://solidity-by-example.org/sending-ether
+        (bool sent, bytes memory data) = msg.sender.call{
+            value: indBalanceRewards
+        }("");
+        require(sent, "Withdrawal failed :( ");
+    }
+
+    /*
+    Allows any user to repatriate "unproductive" funds that are left in the staking contract
+    past the defined withdrawal period
+    */
+
+    function execute() public claimDeadlineReached(true) notCompleted {
+        uint256 contractBalance = address(this).balance;
+        exampleExternalContract.complete{value: address(this).balance}();
+    }
+
+    /*
+  Time to "kill-time" on our local testnet
+  */
+    function killTime() public {
+        currentBlock = block.timestamp;
+    }
+
+    /*
+  \Function for our smart contract to receive ETH
+  cc: https://docs.soliditylang.org/en/latest/contracts.html#receive-ether-function
+  */
+    receive() external payable {
+        emit Received(msg.sender, msg.value);
     }
 }
